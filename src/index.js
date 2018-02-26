475ef3fc65164dafda32ab508ddfc871b68ed2e7
nickprudnik <prudniknick@gmail.com>
2018-02-26 10:23:35 +0300

    'solution'

--- src/index.js
+++ src/index.js
@@ -1,31 +1,47 @@
 class Sorter {
-  constructor() {
-    // your implementation
-  }
 
-  add(element) {
-    // your implementation
-  }
+constructor() {
+this.srr = [];
+this.setComparator((a, b) => a - b);
+}
 
-  at(index) {
-    // your implementation
-  }
+add(element) {
+this.srr.push(element);
+this.length = this.srr.length;
+}
 
-  get length() {
-    // your implementation
-  }
+at(index) {
+return this.srr[index];
+}
 
-  toArray() {
-    // your implementation
-  }
+toArray() {
+var z = [];
+for (var i = 0; i < this.length; i++){
+z[i] = this.srr[i];
+}
+return z;
+}
 
-  sort(indices) {
-    // your implementation
-  }
+sort(indices) {
+var sortirus = [];
+indices.sort();
 
-  setComparator(compareFunction) {
-    // your implementation
-  }
+for (var i = 0; i < indices.length; i++) {
+sortirus.push(this.srr[indices[i]]);
+}
+
+sortirus.sort(this.compareFunction);
+
+for (var i = 0; i < indices.length; i++) {
+this.srr[indices[i]] = sortirus[i];
+}
+
+}
+
+setComparator(compareFunction) {
+this.compareFunction = compareFunction;
+}
+
 }
 
 module.exports = Sorter;
